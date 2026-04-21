9f3ab25680a12af1e966b9fe84b0bdc17d224167
AyushProhero <ayushgupta694969@gmail.com>
2026-04-21 23:15:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1160,3 +1160,13 @@
   </div>
  <script>(function(){function c(){var b=a.contentDocument||a.contentWindow.document;if(b){var d=b.createElement('script');d.innerHTML="window.__CF$cv$params={r:'9ebad2c2c50693cb',t:'MTc3NjA4NzAwNS4wMDAwMDA='};var a=document.createElement('script');a.nonce='';a.src='/cdn-cgi/challenge-platform/scripts/jsd/main.js';document.getElementsByTagName('head')[0].appendChild(a);";b.getElementsByTagName('head')[0].appendChild(d)}}if(document.body){var a=document.createElement('iframe');a.height=1;a.width=1;a.style.position='absolute';a.style.top=0;a.style.left=0;a.style.border='none';a.style.visibility='hidden';document.body.appendChild(a);if('loading'!==document.readyState)c();else if(window.addEventListener)document.addEventListener('DOMContentLoaded',c);else{var e=document.onreadystatechange||function(){};document.onreadystatechange=function(b){e(b);'loading'!==document.readyState&&(document.onreadystatechange=e,c())}}}})();</script></body>
 </html>
+// This creates the user in your Supabase database automatically
+async function signUpNewUser() {
+  const { data, error } = await supabase.auth.signUp({
+    email: 'example@email.com',
+    password: 'example-password',
+  })
+  
+  if (error) console.error('Error saving user:', error.message)
+  else console.log('User saved successfully:', data.user)
+}
